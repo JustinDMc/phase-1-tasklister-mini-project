acc27a0c7a08bf8513b04f37e4595fad1d67b2e2
Justin <jdanielmcarthur@msn.com>
2021-05-25 09:10:54 -0700

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+//Get DOM Elements
+  const input = document.getElementById("new-task-description");
+  const submit = document.getElementById("submit-task");
+  const list = document.getElementById("tasks");
+
+//Write callback functions for event listeners
+  let submitBtnClick = (e) => {
+  e.preventDefault();
+  let text = input.value;
+  let newLi = document.createElement("li");
+  newLi.innerHTML = text;
+
+  list.appendChild(newLi);
+  
+}
+
+//Add event listeners and use functions above
+  submit.addEventListener("click", submitBtnClick)
+})
